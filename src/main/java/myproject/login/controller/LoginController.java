bbb0a5bb07c417c2e56d482433260ab85ddd6026
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-02-04 23:16:44 +0530

    Basic Login Form

---
+++ src/main/java/myproject/login/controller/LoginController.java
@@ -0,0 +1,24 @@
+package myproject.login.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LoginController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String uname = request.getParameter("uname");
+		String upwd = request.getParameter("upwd");
+		ModelAndView mav = null;
+		if(uname.equals("tofique")&& upwd.equals("1234")) {
+			mav = new ModelAndView("success");
+		}else {
+			mav = new ModelAndView("failure");
+		}
+		return mav;
+	}
+
+}
